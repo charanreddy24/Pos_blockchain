78fccf6566200b756ac20090d3e09aa2e790f3c5
Ram Asodhi <ramcharan2510@gmail.com>
2024-02-04 23:20:35 -0500

    changes to CSS file and updated the date that displays on the screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,3 @@
-// App.js
-
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
@@ -10,13 +8,13 @@ const App = () => {
   useEffect(() => {
     fetchData();
   }, [selectedDate]);
-  
+
   const fetchData = () => {
     if (!selectedDate) {
       console.error("Please select a date.");
       return;
     }
-    let domain = window.location.origin; 
+    let domain = window.location.origin;
     let port = 5000;
     let url = `${domain}:${port}/metrics/${selectedDate}`;
     fetch(url)
@@ -38,9 +36,18 @@ const App = () => {
     const day = inputDate.getDate().toString().padStart(2, "0");
     const month = (inputDate.getMonth() + 1).toString().padStart(2, "0");
     const year = inputDate.getFullYear().toString();
-
     const formattedDate = day + month + year;
     setSelectedDate(formattedDate);
+  };
+
+  const handleYesterdayDate = () => {
+    const currentDate = new Date();
+    const day = currentDate.getDate() - 1;
+    const month = currentDate.getMonth() + 1;
+    const year = currentDate.getFullYear();
+    return `${day < 10 ? "0" + day : day}-${
+      month < 10 ? "0" + month : month
+    }-${year}`;
   };
 
   function getYesterdayDate() {
@@ -50,55 +57,57 @@ const App = () => {
     const day = yesterday.getDate().toString().padStart(2, "0");
     const month = (yesterday.getMonth() + 1).toString().padStart(2, "0");
     const year = yesterday.getFullYear().toString();
-    
+
     return day + month + year;
   }
 
   function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
   }
-  
+
   return (
     <div className="app-container">
-      <header className="header">
-        Analyzing stake among validators in PoS blockchains
-      </header>
+      <header className="header">Decentralization in Pos Blockchains</header>
 
       {selectedDate ? (
         <div className="metrics-section">
-        <table className="metrics-table">
-          {tableData && tableData.length > 0 ? (
-            <>
-              <thead>
-                <tr>
-                  <th>Blockchain</th>
-                  <th>Number of validators</th>
-                  <th>Nakamoto Coefficient Safety</th>
-                  <th>Nakamoto coefficient Liveness</th>
-                  <th>gini</th>
-                </tr>
-              </thead>
-              <tbody>
-                {tableData.map((item, index) => (
-                  <tr key={index}>
-                    <td>{capitalizeFirstLetter(item.blockchain)}</td>
-                    <td>{item.N}</td>
-                    <td>{item.nc_safety_percent}% ({item.nc_safety})</td>
-                    <td>{item.nc_liveness_percent}% ({item.nc_liveness})</td>
-                    <td>{item.gini}</td>
+          <table className="metrics-table">
+            {tableData && tableData.length > 0 ? (
+              <>
+                <thead>
+                  <tr>
+                    <th>Blockchain</th>
+                    <th>Number of validators</th>
+                    <th>Nakamoto Coefficient Safety</th>
+                    <th>Nakamoto coefficient Liveness</th>
+                    <th>gini</th>
                   </tr>
-                ))}
-              </tbody>
-            </>
-          ) : (
-            <div>
-              <h3 className="no-data">
-                No data available for the selected date. Please select a date
-                between 24-10-2023 and 26-01-2024
-              </h3>
-            </div>
-          )}
-        </table>
+                </thead>
+                <tbody>
+                  {tableData.map((item, index) => (
+                    <tr key={index}>
+                      <td>{capitalizeFirstLetter(item.blockchain)}</td>
+                      <td>{item.N}</td>
+                      <td>
+                        {item.nc_safety_percent}% ({item.nc_safety})
+                      </td>
+                      <td>
+                        {item.nc_liveness_percent}% ({item.nc_liveness})
+                      </td>
+                      <td>{item.gini}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </>
+            ) : (
+              <div>
+                <h3 className="no-data">
+                  No data available for the selected date. Please select a date
+                  between 24-10-2023 and {handleYesterdayDate()}
+                </h3>
+              </div>
+            )}
+          </table>
         </div>
       ) : (
         <div>
